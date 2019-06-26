afd9d825d4de7d7a83e774bcbcad2b8ad80848f8
yy2274 <yongmeng42@qq.com>
2019-06-26 19:21:33 +0800

    前端首页、酒店搜索显示页面、登录注册页面的完成以及其他页面的部分完成。

---
+++ src/main/java/net/suncaper/demo/service/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package net.suncaper.demo.service;
+
+import net.suncaper.demo.domain.Customer;
+import net.suncaper.demo.domain.CustomerExample;
+import net.suncaper.demo.mapper.CustomerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @Override
+    public List<Customer> findUser(String name) {
+        CustomerExample example = new CustomerExample();
+        if(name != null && !name.equals("")) {
+            example.createCriteria().andNameLike("%" + name + "%");
+        }
+
+        return customerMapper.selectByExample(example);
+    }
+
+    @Override
+    public void saveCustomer(Customer customer) {
+
+      //  if(customer.getId() == null || customer.getId().equals("")) {
+            customerMapper.insert(customer);
+   //     } else {
+   //         customerMapper.updateByPrimaryKey(customer);
+  //      }
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        customerMapper.deleteByPrimaryKey(id);
+    }
+
+
+    public Customer findUserById(int id) {
+        return customerMapper.selectByPrimaryKey(id);
+    }
+}
