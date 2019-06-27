f1fee263d1a759fb21aff3b5daf736766f2a5e5c
dengkanghou <1254933919@qq.com>
2019-06-27 21:14:52 +0800

    初步实现了用户注册和搜索信息控制模块

---
+++ src/main/java/net/suncaper/demo/controller/RegisterController.java
@@ -0,0 +1,35 @@
+package net.suncaper.demo.controller;
+
+import net.suncaper.demo.domain.User;
+import net.suncaper.demo.mapper.UserMapper;
+import net.suncaper.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+//注册控制器
+@Controller
+@RequestMapping({"register"})
+public class RegisterController {
+    @Autowired
+    private UserService userService;
+
+    //显示注册页面
+    @GetMapping("/get")
+    public String RegisterPage(Model model) {
+        System.out.println("1");
+        model.addAttribute("user", new User());
+        return "/register";
+    }
+
+    //待修改
+    //从home接受信息传入此方法 并进行处理 显示在页面上
+    @PostMapping("/post")
+    public String SaveUser(User user, Model model) {
+        System.out.println("2");
+        model.addAttribute("user", new User());
+        userService.saveUser(user);
+        return "redirect:/userorder/get";
+    }
+}
